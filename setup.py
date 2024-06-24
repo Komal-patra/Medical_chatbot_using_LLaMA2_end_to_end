840d3a1180d12ffc6dec9f40a1e3a412f765bc4a
Komal-patra <komalfsds 2022@gmail.com>
2024-06-24 21:45:24 +0100

    setup file added and exceuted to create the local packages

---
+++ setup.py
@@ -0,0 +1,12 @@
+#this file is used to create the local packages
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Komal patra',
+    author_email= 'komalfsds2022@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
